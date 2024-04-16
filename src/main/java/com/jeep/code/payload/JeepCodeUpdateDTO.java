4b15181c11e28a347c7ad744b16cf989a4abe53a
harknoel <bontilao.harknoel@gmail.com>
2024-04-16 21:04:56 +0800

    Refactor jeep code update DTO

--- src/main/java/com/jeep/code/payload/JeepCodeUpdateDTO.java
+++ src/main/java/com/jeep/code/payload/JeepCodeUpdateDTO.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JeepCodeUpdateDTO {
-    String code;
-    String newCode;
+    private String code;
+    private String newCode;
 }
