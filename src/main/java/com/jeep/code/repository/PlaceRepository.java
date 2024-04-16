49660a7beff8e39e71bd021780ac047b5c05fa5c
harknoel <bontilao.harknoel@gmail.com>
2024-04-16 22:12:26 +0800

    Query all places

--- src/main/java/com/jeep/code/repository/PlaceRepository.java
+++ src/main/java/com/jeep/code/repository/PlaceRepository.java
@@ -3,9 +3,14 @@ package com.jeep.code.repository;
 import com.jeep.code.model.JeepCode;
 import com.jeep.code.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
     Optional<Place> findByPlaceNameAndJeepCode(String placeName, JeepCode jeepCode);
+
+    @Query("SELECT DISTINCT p.placeName FROM Place p")
+    List<String> findAllUniquePlaceNames();
 }
