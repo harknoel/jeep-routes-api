beba8162bc2c06637601688c94ef52c0bd4e7609
harknoel <bontilao.harknoel@gmail.com>
2024-04-16 21:04:11 +0800

    Add method to find object through place name and jeep code

--- src/main/java/com/jeep/code/repository/PlaceRepository.java
+++ src/main/java/com/jeep/code/repository/PlaceRepository.java
@@ -1,7 +1,11 @@
 package com.jeep.code.repository;
 
+import com.jeep.code.model.JeepCode;
 import com.jeep.code.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
+    Optional<Place> findByPlaceNameAndJeepCode(String placeName, JeepCode jeepCode);
 }
