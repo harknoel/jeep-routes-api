95cbad83a3a17a60671e65f1b0eaf9f5cb6dca85
harknoel <bontilao.harknoel@gmail.com>
2024-04-16 22:02:45 +0800

    Implement the mapping for getting all codes

--- src/main/java/com/jeep/code/controller/JeepCodeController.java
+++ src/main/java/com/jeep/code/controller/JeepCodeController.java
@@ -1,11 +1,14 @@
 package com.jeep.code.controller;
 
+import com.jeep.code.model.JeepCode;
 import com.jeep.code.payload.JeepCodeAddDTO;
 import com.jeep.code.payload.JeepCodeUpdateDTO;
 import com.jeep.code.service.JeepCodeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/codes")
@@ -14,6 +17,16 @@ public class JeepCodeController {
 
     public JeepCodeController(JeepCodeService jeepCodeService) {
         this.jeepCodeService = jeepCodeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<String>> getAllJeepCodes() {
+        List<String> allJeepCodes = jeepCodeService.getAllJeepCodes();
+        if (allJeepCodes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(allJeepCodes);
+        }
     }
 
     @PostMapping("/add")
