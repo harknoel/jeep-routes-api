9d191b49af89f1da51c3af67795f85ce7744e665
harknoel <bontilao.harknoel@gmail.com>
2024-04-16 22:02:12 +0800

    Query all code

--- src/main/java/com/jeep/code/repository/JeepCodeRepository.java
+++ src/main/java/com/jeep/code/repository/JeepCodeRepository.java
@@ -2,9 +2,15 @@ package com.jeep.code.repository;
 
 import com.jeep.code.model.JeepCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JeepCodeRepository extends JpaRepository<JeepCode, Integer> {
     Optional<JeepCode> findByCode(String code);
+
+    @Query("SELECT j.code FROM JeepCode j")
+    List<String> findAllCodes();
+
 }
